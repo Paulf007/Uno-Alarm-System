c254173c1b4855f1c6700c96d32c6ab649a86d56
Paulf007 <paul@paulf.co.za>
2019-11-26 06:01:27 +0200

    Done
    Done

--- a_mqtt.ino
+++ a_mqtt.ino
@@ -110,5 +110,120 @@ void check_mqtt (){
 }
 
 
+<<<<<<< HEAD
 
   
+=======
+void sendStatusData(byte stat){
+//  Serial.print("MQTT Command Recv:");
+//  Serial.println(stat);
+
+  
+  if (stat == 49){ // 49 is the byte version of 1
+  // Serial.println("Case 1 Run");
+    StaticJsonDocument<100> doc;
+      JsonObject Status  = doc.createNestedObject("Status");  
+      Status["Module"] = 1;
+      JsonArray  FriendlyName  = Status.createNestedArray("FriendlyName");
+      FriendlyName.add(Fname);
+       Status["Topic"] = topic;  
+      char buffer[80];
+      serializeJson(doc, buffer);
+      size_t n = serializeJson(doc, buffer);
+      client.publish(statTopic, buffer,n);
+     //Serial.print("freeMemory()=");
+   // Serial.println(freeMemory());
+    doc.clear();
+    //checkMemory();
+  } else if (stat == 53){ // is the byte version of 5
+      //Serial.println("Case 5 Run");
+      StaticJsonDocument<50> doc5;
+      JsonObject Status5  = doc5.createNestedObject("StatusNET");  
+      Status5["IPAddress"] =ip2CharArray(Ethernet.localIP());
+      Status5["Mac"] = macS;
+      char buffer5[75];
+      serializeJson(doc5, buffer5);
+      size_t n5 = serializeJson(doc5, buffer5);
+      client.publish(status_5, buffer5,n5);
+    //Serial.print("freeMemory()=");
+    //Serial.println(freeMemory());
+   // checkMemory();
+    doc5.clear();
+  }
+   // Serial.println(stat);
+    
+  }
+  
+void sensorData (){
+  StaticJsonDocument<80> doc;
+//doc["ver"] = ver;
+//doc["ip"] = ip2CharArray(Ethernet.localIP());
+JsonObject Status  = doc.createNestedObject("DS18B20");  
+Status["temp"] = dssensor ();
+doc["ldr"] = analogRead(photocellPin); ;
+//doc["dev"] = devName;
+//doc["con"] = connect_timeout;
+//doc["ave"] = average;
+
+  char buffer[80];
+  serializeJson(doc, buffer);
+ // Serial.println("Sending message to MQTT topic..");
+ // serializeJson(doc, Serial);
+ // Serial.println();
+ size_t n = serializeJson(doc, buffer);
+client.publish(sensorTopic, buffer,n);
+ doc.clear();
+}
+
+void send_tele_state (){
+
+ StaticJsonDocument<20> doc;
+   doc["ver"] = ver;
+   char buffer[20];
+  serializeJson(doc, buffer);
+ size_t n = serializeJson(doc, buffer);
+client.publish(recState, buffer,n);  
+ doc.clear();
+}
+
+void send_data (){
+
+   if (millis()- newTmqtt >= 300000) {
+    send_tele_state ();
+    sensorData () ;
+
+// sendData (average,rpm,maxRpm);
+newTmqtt = millis();
+       //testJson ();
+    }
+  
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+/*  
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+*/
+   
+if(strcmp(topic, cmdTopic) == 0){
+  //Serial.print ("Command topic recived:");
+  //Serial.println((char)payload[0]);
+  //byte a = (char)payload[0];
+  sendStatusData((char)payload[0]) ; 
+  if ((char)payload[0] == '1'){
+    sendStatusData('1');
+  } else if ((char)payload[0] == '5'){
+   sendStatusData('5') ; 
+   //Serial.println("Data 5 Sent");
+  }
+}
+
+
+
+}
+>>>>>>> 8f2e0c8db037f6e75938254d4b3a73d1112570b0
